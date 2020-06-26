dc535c2d9f5a6ed1eed17fb96c105906859cc950
shaileshpilare <shailesh.pilare@broadcom.com>
2020-06-25 19:08:08 -0700

    NYC Data Download script

---
+++ Project-Scripts/Download_NYC_Data.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+wget -O /mnt1/DataSet/yellow_tripdata_2009-01.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-01.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2009-02.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-02.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2009-03.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-03.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2009-04.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-04.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2009-05.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-05.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2009-06.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-06.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2009-07.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-07.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2009-08.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-08.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2009-09.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-09.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2009-10.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-10.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2009-11.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-11.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2009-12.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-12.csv
+
+wget -O /mnt1/DataSet/yellow_tripdata_2010-01.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-01.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2010-02.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-02.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2010-03.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-03.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2010-04.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-04.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2010-05.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-05.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2010-06.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-06.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2010-07.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-07.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2010-08.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-08.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2010-09.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-09.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2010-10.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-10.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2010-11.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-11.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2010-12.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2010-12.csv
+
+wget -O /mnt1/DataSet/yellow_tripdata_2011-01.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-01.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2011-02.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-02.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2011-03.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-03.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2011-04.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-04.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2011-05.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-05.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2011-06.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-06.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2011-07.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-07.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2011-08.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-08.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2011-09.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-09.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2011-10.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-10.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2011-11.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-11.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2011-12.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2011-12.csv
+
+wget -O /mnt1/DataSet/yellow_tripdata_2012-01.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-01.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2012-02.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-02.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2012-03.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-03.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2012-04.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-04.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2012-05.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-05.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2012-06.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-06.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2012-07.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-07.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2012-08.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-08.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2012-09.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-09.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2012-10.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-10.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2012-11.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-11.csv
+wget -O /mnt1/DataSet/yellow_tripdata_2012-12.csv https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2012-12.csv
