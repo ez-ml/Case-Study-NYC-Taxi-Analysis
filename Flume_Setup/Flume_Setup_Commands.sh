96b8517d0d2c31498d2b3f5050146c447ff563f0
shaileshpilare <shailesh.pilare@broadcom.com>
2020-07-21 18:17:56 -0700

    Flume Changes for Conda

--- Flume_Setup/Flume_Setup_Commands.sh
+++ Flume_Setup/Flume_Setup_Commands.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
 docker build -t hishailesh77/flume:latest .
 docker push hishailesh77/flume
-kubectl apply -f flume.yaml
+kubectl apply -f Flume_Deployment.yaml
 
+#Command To Delete Docker
+#docker system prune -a
